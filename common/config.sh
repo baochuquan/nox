ddadb1de235733aaeaa00bb53be7f81edeeaa7bf
baocq <baocq@fenbi.com>
2020-11-12 00:35:05 +0800

    FIX: config.sh

--- common/config.sh
+++ common/config.sh
@@ -11,36 +11,3 @@ function ldap() {
     fi
     echo $ldap
 }
-
-function jira_token() {
-    local configFile="$NOX_CONFIG/config.yaml"
-    local token=`yq r $configFile jira-token`
-    if [[ -z $token ]]; then
-        error "The value of key \`jira-token\` in config.yaml is not initialized. Please initialize it. See example in \`$NOX_TEMPLATES/config-template.yaml.\`"
-        echo "API Token 获取方式：\`https://jira.zhenguanyu.com\` -> 点击右上角用户名 -> 点击【 Configure 】-> 点击【 Add New Token 】"
-        exit 1
-    fi
-    echo $token
-}
-
-function jenkins_token() {
-    local configFile="$NOX_CONFIG/config.yaml"
-    local token=`yq r $configFile jenkins-token`
-    if [[ -z $token ]]; then
-        error "The value of key \`jenkins-token\` in config.yaml is not initialized. Please initialize it. See example in \`$NOX_TEMPLATES/config-template.yaml.\`"
-        echo "API Token 获取方式：\`https://build.zhenguanyu.com\` -> 点击右上角用户头像 -> 选择【 API Token Authentication 】-> 点击【 New API Token 】按钮"
-        exit 1
-    fi
-    echo $token
-}
-
-function gerrit_token() {
-    local configFile="$NOX_CONFIG/config.yaml"
-    local token=`yq r $configFile gerrit-token`
-    if [[ -z $token ]]; then
-        error "The value of key \`gerrit-token\` in config.yaml is not initialized. Please initialize it. See example in \`$NOX_TEMPLATES/config-template.yaml.\`"
-        echo "API Token 获取方式：\`https://gerrit.zhenguanyu.com\` -> 点击右上角用户名 -> 选择 HTTP Credentials -> 点击【 GENERATED NEW PASSWORD 】按钮"
-        exit 1
-    fi
-    echo $token
-}
