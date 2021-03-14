fed620e5ff18ddd9aba4932daa103264b97df110
baocq <baocq@fenbi.com>
2021-03-14 15:18:23 +0800

    BUILD: update nox

--- scripts/poker/king.sh
+++ scripts/poker/king.sh
@@ -2,30 +2,28 @@
 
 ##############################################################################
 ##
-##  Filename: ace.sh
+##  Filename: king.sh
 ##  Author: baocq
-##  E-mail: baocq@fenbi.com
-##  Date: Sun Nov  8 15:27:20 CST 2020
+##  E-mail: 
+##  Date: Sun Mar 14 15:11:48 CST 2021
 ##
 ##############################################################################
 
 source $NOX_COMMON/utils.sh
 source $NOX_COMMON/config.sh
 
-# Usage of ace.sh
-function _usage_of_ace() {
+# Usage of king.sh
+function _usage_of_king() {
     cat << EOF
 Usage:
-    ace [--count <count>] [-r]
+    king 
 
 Description:
-    ace 相关功能
+    打印 K ....
 
 Option:
     --help|-h:                                          -- 使用帮助
     --debug|-x:                                         -- 调试模式
-    --reverse|-r:                                       -- 是否逆序打印，无改选项则正序打印
-    --count|-c:                                         -- 打印次数，无该选项则打印一遍
 
 EOF
 }
@@ -45,12 +43,10 @@ EOF
 #       mkdir -f 会执行失败，因为 -f 会被 mkdir 当做选项来解析
 #       mkdir -- -f 会执行成功，-f 不会被当做选项
 
-function ace() {
+function king() {
     local debug=0
-    local reverse=false
-    local count=1
 
-    local ARGS=`ggetopt -o hxrc: --long help,debug,reverse,count: -n 'Error' -- "$@"`
+    local ARGS=`ggetopt -o hx --long help,debug -n 'Error' -- "$@"`
     if [ $? != 0 ]; then
         error "Invalid option..." >&2;
         exit 1;
@@ -61,20 +57,12 @@ function ace() {
     while true ; do
         case "$1" in
             -h|--help)
-                _usage_of_ace
+                _usage_of_king
                 exit 1
                 ;;
             -x|--debug)
                 debug=1
                 shift
-                ;;
-            -r|--reverse)
-                reverse=true
-                shift
-                ;;
-            -c|--count)
-                count=$2
-                shift 2
                 ;;
             --)
                 shift
@@ -92,23 +80,8 @@ function ace() {
     fi
 
     # start
-    local poker=(A 2 3 4 5 6 7 8 9 10 J Q K Joker)
-    
-    if [[ $reverse == true ]]; then
-        local length=${#poker[*]}
-        local tmp=($poker)
-        local i=0
-        while [[ $i -lt $length ]]; do
-            poker[$[$i + 1]]=$tmp[$[$length - $i]]
-            i=$[i + 1]
-        done
-    fi
-
-    local j=0
-    while [[ $j -lt $count ]]; do
-        echo $poker
-        j=$[j + 1]
-    done
+    success "Demo Command: nox poker king"
+    warning "KING!!!"
 
     if [[ $debug == 1 ]]; then
         set +x
@@ -116,4 +89,4 @@ function ace() {
 }
 
 # Execute current script
-ace $*
+king $*
